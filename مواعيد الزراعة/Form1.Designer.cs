d3905a6ad01b87feae437fa1db9d0d3fb80f8065
Abdullah Saleh AlSaleh <abdalla.aa5516@hotmail.com>
2022-02-05 17:28:07 +0300

    Add project files.

---
+++ مواعيد الزراعة/Form1.Designer.cs
@@ -0,0 +1,437 @@
+﻿
+namespace Agricultural_Calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.D3 = new System.Windows.Forms.Label();
+            this.Month4 = new System.Windows.Forms.Label();
+            this.Month3 = new System.Windows.Forms.Label();
+            this.Month1 = new System.Windows.Forms.Label();
+            this.Month2 = new System.Windows.Forms.Label();
+            this.T1 = new System.Windows.Forms.Label();
+            this.T2 = new System.Windows.Forms.Label();
+            this.T3 = new System.Windows.Forms.Label();
+            this.T4 = new System.Windows.Forms.Label();
+            this.D4 = new System.Windows.Forms.Label();
+            this.D2 = new System.Windows.Forms.Label();
+            this.D1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.D7 = new System.Windows.Forms.Label();
+            this.Month8 = new System.Windows.Forms.Label();
+            this.T8 = new System.Windows.Forms.Label();
+            this.D8 = new System.Windows.Forms.Label();
+            this.Month7 = new System.Windows.Forms.Label();
+            this.D5 = new System.Windows.Forms.Label();
+            this.D6 = new System.Windows.Forms.Label();
+            this.T7 = new System.Windows.Forms.Label();
+            this.Month6 = new System.Windows.Forms.Label();
+            this.T6 = new System.Windows.Forms.Label();
+            this.Month5 = new System.Windows.Forms.Label();
+            this.T5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.D12 = new System.Windows.Forms.Label();
+            this.D10 = new System.Windows.Forms.Label();
+            this.T12 = new System.Windows.Forms.Label();
+            this.Month12 = new System.Windows.Forms.Label();
+            this.Month9 = new System.Windows.Forms.Label();
+            this.Month11 = new System.Windows.Forms.Label();
+            this.T9 = new System.Windows.Forms.Label();
+            this.T11 = new System.Windows.Forms.Label();
+            this.D9 = new System.Windows.Forms.Label();
+            this.D11 = new System.Windows.Forms.Label();
+            this.Month10 = new System.Windows.Forms.Label();
+            this.T10 = new System.Windows.Forms.Label();
+            this.datey = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.eToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            resources.ApplyResources(this.showToolStripMenuItem, "showToolStripMenuItem");
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // eToolStripMenuItem
+            // 
+            this.eToolStripMenuItem.Name = "eToolStripMenuItem";
+            resources.ApplyResources(this.eToolStripMenuItem, "eToolStripMenuItem");
+            this.eToolStripMenuItem.Click += new System.EventHandler(this.eToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.D3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Month4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Month3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Month1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Month2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.T1, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.T2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.T3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.T4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.D4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.D2, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.D1, 3, 2);
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // D3
+            // 
+            resources.ApplyResources(this.D3, "D3");
+            this.D3.Name = "D3";
+            // 
+            // Month4
+            // 
+            resources.ApplyResources(this.Month4, "Month4");
+            this.Month4.Name = "Month4";
+            // 
+            // Month3
+            // 
+            resources.ApplyResources(this.Month3, "Month3");
+            this.Month3.Name = "Month3";
+            // 
+            // Month1
+            // 
+            resources.ApplyResources(this.Month1, "Month1");
+            this.Month1.Name = "Month1";
+            this.Month1.Click += new System.EventHandler(this.Month1_Click);
+            // 
+            // Month2
+            // 
+            resources.ApplyResources(this.Month2, "Month2");
+            this.Month2.Name = "Month2";
+            // 
+            // T1
+            // 
+            resources.ApplyResources(this.T1, "T1");
+            this.T1.ForeColor = System.Drawing.Color.White;
+            this.T1.Name = "T1";
+            // 
+            // T2
+            // 
+            resources.ApplyResources(this.T2, "T2");
+            this.T2.Name = "T2";
+            // 
+            // T3
+            // 
+            resources.ApplyResources(this.T3, "T3");
+            this.T3.Name = "T3";
+            // 
+            // T4
+            // 
+            resources.ApplyResources(this.T4, "T4");
+            this.T4.Name = "T4";
+            // 
+            // D4
+            // 
+            resources.ApplyResources(this.D4, "D4");
+            this.D4.Name = "D4";
+            // 
+            // D2
+            // 
+            resources.ApplyResources(this.D2, "D2");
+            this.D2.Name = "D2";
+            // 
+            // D1
+            // 
+            resources.ApplyResources(this.D1, "D1");
+            this.D1.Name = "D1";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.D7, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Month8, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.T8, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.D8, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Month7, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.D5, 3, 2);
+            this.tableLayoutPanel2.Controls.Add(this.D6, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.T7, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Month6, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.T6, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Month5, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.T5, 3, 1);
+            this.tableLayoutPanel2.ForeColor = System.Drawing.Color.White;
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // D7
+            // 
+            resources.ApplyResources(this.D7, "D7");
+            this.D7.Name = "D7";
+            // 
+            // Month8
+            // 
+            resources.ApplyResources(this.Month8, "Month8");
+            this.Month8.Name = "Month8";
+            // 
+            // T8
+            // 
+            resources.ApplyResources(this.T8, "T8");
+            this.T8.Name = "T8";
+            // 
+            // D8
+            // 
+            resources.ApplyResources(this.D8, "D8");
+            this.D8.Name = "D8";
+            // 
+            // Month7
+            // 
+            resources.ApplyResources(this.Month7, "Month7");
+            this.Month7.Name = "Month7";
+            // 
+            // D5
+            // 
+            resources.ApplyResources(this.D5, "D5");
+            this.D5.Name = "D5";
+            // 
+            // D6
+            // 
+            resources.ApplyResources(this.D6, "D6");
+            this.D6.Name = "D6";
+            // 
+            // T7
+            // 
+            resources.ApplyResources(this.T7, "T7");
+            this.T7.Name = "T7";
+            // 
+            // Month6
+            // 
+            resources.ApplyResources(this.Month6, "Month6");
+            this.Month6.Name = "Month6";
+            // 
+            // T6
+            // 
+            resources.ApplyResources(this.T6, "T6");
+            this.T6.Name = "T6";
+            // 
+            // Month5
+            // 
+            resources.ApplyResources(this.Month5, "Month5");
+            this.Month5.Name = "Month5";
+            // 
+            // T5
+            // 
+            resources.ApplyResources(this.T5, "T5");
+            this.T5.Name = "T5";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
+            this.tableLayoutPanel3.Controls.Add(this.D12, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.D10, 2, 2);
+            this.tableLayoutPanel3.Controls.Add(this.T12, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.Month12, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Month9, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Month11, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.T9, 3, 1);
+            this.tableLayoutPanel3.Controls.Add(this.T11, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.D9, 3, 2);
+            this.tableLayoutPanel3.Controls.Add(this.D11, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.Month10, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.T10, 2, 1);
+            this.tableLayoutPanel3.ForeColor = System.Drawing.Color.White;
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            // 
+            // D12
+            // 
+            resources.ApplyResources(this.D12, "D12");
+            this.D12.Name = "D12";
+            // 
+            // D10
+            // 
+            resources.ApplyResources(this.D10, "D10");
+            this.D10.Name = "D10";
+            // 
+            // T12
+            // 
+            resources.ApplyResources(this.T12, "T12");
+            this.T12.Name = "T12";
+            // 
+            // Month12
+            // 
+            resources.ApplyResources(this.Month12, "Month12");
+            this.Month12.Name = "Month12";
+            // 
+            // Month9
+            // 
+            resources.ApplyResources(this.Month9, "Month9");
+            this.Month9.Name = "Month9";
+            // 
+            // Month11
+            // 
+            resources.ApplyResources(this.Month11, "Month11");
+            this.Month11.Name = "Month11";
+            // 
+            // T9
+            // 
+            resources.ApplyResources(this.T9, "T9");
+            this.T9.Name = "T9";
+            // 
+            // T11
+            // 
+            resources.ApplyResources(this.T11, "T11");
+            this.T11.Name = "T11";
+            // 
+            // D9
+            // 
+            resources.ApplyResources(this.D9, "D9");
+            this.D9.Name = "D9";
+            // 
+            // D11
+            // 
+            resources.ApplyResources(this.D11, "D11");
+            this.D11.Name = "D11";
+            // 
+            // Month10
+            // 
+            resources.ApplyResources(this.Month10, "Month10");
+            this.Month10.Name = "Month10";
+            // 
+            // T10
+            // 
+            resources.ApplyResources(this.T10, "T10");
+            this.T10.Name = "T10";
+            // 
+            // datey
+            // 
+            resources.ApplyResources(this.datey, "datey");
+            this.datey.ForeColor = System.Drawing.Color.White;
+            this.datey.Name = "datey";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.Controls.Add(this.datey);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label Month1;
+        private System.Windows.Forms.Label T1;
+        private System.Windows.Forms.Label D1;
+        private System.Windows.Forms.Label Month2;
+        private System.Windows.Forms.Label T2;
+        private System.Windows.Forms.Label D2;
+        private System.Windows.Forms.Label D3;
+        private System.Windows.Forms.Label Month4;
+        private System.Windows.Forms.Label Month3;
+        private System.Windows.Forms.Label T3;
+        private System.Windows.Forms.Label T4;
+        private System.Windows.Forms.Label D4;
+        private System.Windows.Forms.Label D7;
+        private System.Windows.Forms.Label Month8;
+        private System.Windows.Forms.Label T8;
+        private System.Windows.Forms.Label D8;
+        private System.Windows.Forms.Label Month7;
+        private System.Windows.Forms.Label D5;
+        private System.Windows.Forms.Label D6;
+        private System.Windows.Forms.Label T7;
+        private System.Windows.Forms.Label Month6;
+        private System.Windows.Forms.Label T6;
+        private System.Windows.Forms.Label Month5;
+        private System.Windows.Forms.Label T5;
+        private System.Windows.Forms.Label D12;
+        private System.Windows.Forms.Label D10;
+        private System.Windows.Forms.Label T12;
+        private System.Windows.Forms.Label Month12;
+        private System.Windows.Forms.Label Month9;
+        private System.Windows.Forms.Label Month11;
+        private System.Windows.Forms.Label T9;
+        private System.Windows.Forms.Label T11;
+        private System.Windows.Forms.Label D9;
+        private System.Windows.Forms.Label D11;
+        private System.Windows.Forms.Label Month10;
+        private System.Windows.Forms.Label T10;
+        private System.Windows.Forms.Label datey;
+    }
+}
+
---
+++ مواعيد الزراعة/Heartbeat.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Agricultural_Calendar
+{
+   public  class Heartbeat
+    { 
+        private readonly System.Timers.Timer _timer;
+        public Heartbeat()
+        {
+            _timer = new System.Timers.Timer(600500) { AutoReset=true};
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+            new Task(() => {
+                while (true)
+                {
+                    NotifyIcon notifyIcon1 = new NotifyIcon();
+                    CultureInfo AE = CultureInfo.GetCultureInfo("ar-AE");
+                    CultureInfo SA = CultureInfo.GetCultureInfo("ar-SA");
+                    HijriCalendar hijriCalendar = new HijriCalendar();
+
+                    // string text = DateTime.Now.ToString("yyyy", SA);
+                    int NewYear = hijriCalendar.GetYear(Convert.ToDateTime(DateTime.Now.AddYears(1)));
+
+                    int Now = hijriCalendar.GetYear(DateTime.Now);
+                    DateTime IFDATE1 = new DateTime(Now, 12, 2, hijriCalendar);
+                    DateTime IFDATE2 = new DateTime(Now, 12, 3, hijriCalendar);
+                    int IfDates1 = Convert.ToInt32(IFDATE1.ToString("yyyyMMdd"));
+                    int IfDates2 = Convert.ToInt32(IFDATE2.ToString("yyyyMMdd"));
+                    if (IfDates2 < IfDates1)
+                    {
+
+                        DateTime dt1 = new DateTime(NewYear, 1, 1, hijriCalendar);
+                        DateTime dt2 = new DateTime(NewYear, 2, 1, hijriCalendar);
+                        DateTime dt3 = new DateTime(NewYear, 3, 2, hijriCalendar);
+                        DateTime dt4 = new DateTime(NewYear, 4, 1, hijriCalendar);
+                        DateTime dt5 = new DateTime(NewYear, 5, 2, hijriCalendar);
+                        DateTime dt6 = new DateTime(NewYear, 6, 2, hijriCalendar);
+                        DateTime dt7 = new DateTime(NewYear, 7, 2, hijriCalendar);
+                        DateTime dt8 = new DateTime(NewYear, 8, 2, hijriCalendar);
+                        DateTime dt9 = new DateTime(NewYear, 9, 1, hijriCalendar);
+                        DateTime dt10 = new DateTime(NewYear, 10, 1, hijriCalendar);
+                        DateTime dt11 = new DateTime(NewYear, 11, 1, hijriCalendar);
+                        DateTime dt12 = new DateTime(NewYear, 12, 1, hijriCalendar);
+
+
+                        var Date1 = new List<Date>()
+            {
+                new Date(){Id=1,Month1=dt1,Month2=dt2,Month3=dt3,Month4=dt4,Month5=dt5,Month6=dt6,Month7=dt7,Month8=dt8,Month9=dt9,Month10=dt10,Month11=dt11,Month12=dt12}
+
+
+            };
+
+                        foreach (var item in Date1)
+                        {
+
+                            int DateNow, ToInt1, ToInt2, ToInt3, ToInt4, ToInt5, ToInt6, ToInt7, ToInt8, ToInt9, ToInt10, ToInt11, ToInt12;
+                            DateNow = Convert.ToInt32(DateTime.Now.ToString("yyyyMMdd"));
+                            ToInt1 = Convert.ToInt32(item.Month1.ToString("yyyyMMdd"));
+                            ToInt2 = Convert.ToInt32(item.Month2.ToString("yyyyMMdd"));
+                            ToInt3 = Convert.ToInt32(item.Month3.ToString("yyyyMMdd"));
+                            ToInt4 = Convert.ToInt32(item.Month4.ToString("yyyyMMdd"));
+                            ToInt5 = Convert.ToInt32(item.Month5.ToString("yyyyMMdd"));
+                            ToInt6 = Convert.ToInt32(item.Month6.ToString("yyyyMMdd"));
+                            ToInt7 = Convert.ToInt32(item.Month7.ToString("yyyyMMdd"));
+                            ToInt8 = Convert.ToInt32(item.Month8.ToString("yyyyMMdd"));
+                            ToInt9 = Convert.ToInt32(item.Month9.ToString("yyyyMMdd"));
+                            ToInt10 = Convert.ToInt32(item.Month10.ToString("yyyyMMdd"));
+                            ToInt11 = Convert.ToInt32(item.Month11.ToString("yyyyMMdd"));
+                            ToInt12 = Convert.ToInt32(item.Month12.ToString("yyyyMMdd"));
+
+
+                            if (DateNow == ToInt1)
+                            {
+
+                                //this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month1.ToString("MMM"), "شهر جديد ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt2)
+                            {
+                                //  this.Hide();
+
+                                notifyIcon1.ShowBalloonTip(1000, item.Month2.ToString("MMM"), "فلفل ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt3)
+                            {
+                                //  this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month3.ToString("MMM"), "الملوخية,شمام ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt4)
+                            {
+                                //  this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month4.ToString("MMM"), "شهر جديد  ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt5)
+                            {
+                                // this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month5.ToString("MMM"), "الملوخية ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt6)
+                            {
+                                //this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month6.ToString("MMM"), "شهر جديد  ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt7)
+                            {
+                                //  this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month7.ToString("MMM"), "شهر جديد  ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt8)
+                            {
+                                //this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month8.ToString("MMM"), "الفراولة,خيار,الزهرة,الملفوف", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt9)
+                            {
+                                // this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month9.ToString("MMM"), "طماطم و باذنجان و فلفل ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt10)
+                            {
+                                // this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month10.ToString("MMM"), "كزبرة بقدونس ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt11)
+                            {
+                                //  this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month11.ToString("MMM"), "شمندر سبائخ ", ToolTipIcon.Info);
+                            }
+                            if (DateNow == ToInt12)
+                            {
+                                //  this.Hide();
+                                notifyIcon1.ShowBalloonTip(1000, item.Month12.ToString("MMM"), "شهر جديد  ", ToolTipIcon.Info);
+                            }
+
+                        }
+                    }
+                }
+            
+            
+            });
+
+      
+
+        }
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop(); 
+        }
+    }
+}
